a82c3cfb548ae392ae85c8e141d0077b3cb6b1fd
RamiresMatias <ramiresmatias20000@gmail.com>
2024-09-18 17:45:50 -0300

    Add remove hooks config nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -101,18 +101,5 @@ export default defineNuxtConfig({
   delayHydration: { 
     mode: 'init',
     debug: process.env.NODE_ENV === 'development'
-  },
-  hooks: {
-    'build:manifest': (manifest) => {
-      const css = manifest['node_modules/nuxt/dist/app/entry.js']?.css
-
-      if (css) {
-
-        for (let i = css.length - 1; i >= 0; i--) {
-
-          if (css[i].startsWith('entry')) css.splice(i, 1)
-        }
-      }
-    },
-  },
+  }
 })
